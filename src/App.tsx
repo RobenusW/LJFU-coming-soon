1d6bfcb380c9c7c6752475db089b734444b5db1c
RobenusW <robenuswork@gmail.com>
2025-01-06 15:19:24 -0500

    commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+// App.jsx or App.js
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="content card shadow-lg">
+        <h1>LetJobsFindYou.com Coming Soon!</h1>
+        <p>
+          A job search platform where businesses can find the right talent and
+          talent post their resumes.
+        </p>
+        <p style={{ fontSize: "0.7rem", fontStyle: "italic" }}>
+          We are working hard to bring you the best job search experience.
+          Please check back soon!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
